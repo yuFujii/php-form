ef7d4c2e94a058d23c71c306675f394a1f04fafc
yuFujii <32005523+yuFujii@users.noreply.github.com>
2018-04-09 16:08:39 +0900

    add getSql

--- module/database.php
+++ module/database.php
@@ -3,3 +3,14 @@ define("DB_HOST", 'host');
 define("DB_USER", 'username');
 define("DB_PASS", 'password');
 define("DB_NAME", 'php-form');
+
+
+function getSqlDatabase()
+{
+    $db = new PDO(
+        'mysql:dbname=' . DB_NAME . ';host=' . DB_HOST . ';charset=utf8',
+        DB_USER,
+        DB_PASS
+    );
+    return $db;
+}
