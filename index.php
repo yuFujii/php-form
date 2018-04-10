30fe6eb4a0ee16a503782c2b3812ef101128ea0a
yuFujii <32005523+yuFujii@users.noreply.github.com>
2018-04-10 11:16:20 +0900

    validation update

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
 <?php
 require_once './module/database.php';
     $db = getSqlDatabase();
+require_once './vendor/autoload.php';
+use Respect\Validation\Validator as v;
 ?>
 
 <?php
@@ -25,7 +27,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } else {
         $name = test_input($_POST["name"]);
         // check if name only contains letters and whitespace
-        if (!preg_match("/^[a-zA-Z ]*$/", $name)) {
+        if (!preg_match("/^[a-zA-Zぁ-んァ-ヶー一-龠]*$/", $name)) {
             $nameErr = "Only letters and white space allowed";
         } else {
             $numOfCorrectInput++;
@@ -37,10 +39,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } else {
         $email = test_input($_POST["email"]);
         // check if e-mail address is well-formed
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $emailErr = "Invalid email format";
-        } else {
+        if (v::email()->validate($email)) {
             $numOfCorrectInput++;
+        } else {
+            $emailErr = "Invalid email format";
         }
     }
         
@@ -50,10 +52,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } else {
         $website = test_input($_POST["website"]);
         // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
-        if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $website)) {
-            $websiteErr = "Invalid URL";
-        } else {
+        if (v::url()->validate($website)) {
             $numOfCorrectInput++;
+        } else {
+            $websiteErr = "Invalid URL";
         }
     }
 
